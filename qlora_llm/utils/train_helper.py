fix bug in gradient accumulation

qlora_llm/utils/train_helper.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Michael Hu.
+# Copyright (c) 2024 sanchi bansal.
 # This project is released under the MIT License.
 # See the accompanying LICENSE file for details.
 
@@ -9,13 +9,6 @@ import torch
 from torch import nn
 import torch.distributed as dist
 import bitsandbytes as bnb
-
-# support running without installing as a package
-from pathlib import Path
-import sys
-
-wd = Path(__file__).parent.parent.resolve()
-sys.path.append(str(wd))
 
 
 def create_trace_profiler(tb_trace_dir: str) -> torch.profiler.profile: